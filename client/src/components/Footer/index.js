547a69bb3f3d52556bd65674011fa5ea1a7de5b8
rva-mike <mike.levy.rva@gmail.com>
2022-10-28 14:02:15 -0400

    css changes, pushing before some tests

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-dark p-4 text-white">
-      <div className="container">
-        &copy;{new Date().getFullYear()} by Lernantino
+      <div className="container footer-text">
+        &copy;{new Date().getFullYear()} Built By React |
+        &nbsp;<FaGithub/>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
