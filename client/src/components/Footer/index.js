efea54fb44420ce19ddbf987c21badd6764ccc90
rva-mike <mike.levy.rva@gmail.com>
2022-10-28 18:51:52 -0400

    link to react website

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-dark p-3 text-white">
       <div className="container footer-text">
-        &copy;{new Date().getFullYear()} | Built with React |
+        &copy;{new Date().getFullYear()} |&nbsp;<a target="_blank" class='gitlink' href='https://reactjs.org/'>Built with React</a>&nbsp;|
         &nbsp;<a target="_blank" rel="noopener noreferrer" href='https://github.com/rva-mike/Substitute-Finder'><FaGithub className='github-icon'/></a>
       </div>
     </footer>
