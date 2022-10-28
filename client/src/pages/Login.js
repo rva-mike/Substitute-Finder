dd1f234b6bad94af55da201b732a2e8d3b6cf2ba
rva-mike <mike.levy.rva@gmail.com>
2022-10-28 16:41:43 -0400

    hover changes, login signup changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,9 @@ import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
+// import Button from '@mui/material/Button';
+
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
@@ -64,7 +67,7 @@ const Login = (props) => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className="btn d-block w-100" type="submit">
+              <button variant="contained" className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
