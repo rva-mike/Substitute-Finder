3c65c28dcbed76ff1efbe2c3ee702c765ed6439c
rva-mike <mike.levy.rva@gmail.com>
2022-11-01 11:33:12 -0400

    applicant change

--- client/src/components/ApplicantList/index.js
+++ client/src/components/ApplicantList/index.js
@@ -3,27 +3,30 @@ import { Link } from "react-router-dom";
 
 const ApplicantList = ({ applications }) => {
   return (
-    <div className="application-cards">
-      {applications.map((application) => (
-        <div className="card">
-          <h5 className="card-header" key={application._id}>
-            <Link
-              to={`/profile/${application.username}`}
-              style={{ fontWeight: 700 }}
-            >
-              {application.username}
-            </Link>
-          </h5>
-          <p className="m-2">
-            Email: {application.email}
-            <br />
-            Phone: {application.phone}
-            <br />
-            Has Degree: {application.degree}
-          </p>
-        </div>
-      ))}
-    </div>
+    <>
+      <h3 className="">Applicants</h3>
+      <div className="application-cards">
+        {applications.map((application) => (
+          <div className="card mr-2">
+            <h5 className="card-header" key={application._id}>
+              <Link
+                to={`/profile/${application.username}`}
+                style={{ fontWeight: 700 }}
+              >
+                {application.username}
+              </Link>
+            </h5>
+            <p className="m-2">
+              Email: {application.email}
+              <br />
+              Phone: {application.phone}
+              <br />
+              Has Degree: {application.degree}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
