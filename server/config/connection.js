e697ad7ce129cc1c92349798924eb470f37ace78
rva-mike <mike.levy.rva@gmail.com>
2022-11-02 10:25:38 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,10 +3,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Substitute-Finder',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sub-atomic-mern',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
