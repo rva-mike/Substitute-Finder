7e8c6a559fe722eb420f3c580e0b43184de3b4c4
rva-mike <mike.levy.rva@gmail.com>
2022-11-02 09:56:13 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sub-atomic-mern',
   {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
+   
   }
 );
 
